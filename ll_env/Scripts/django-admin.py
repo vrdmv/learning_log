6c6e8e718a6a57eda025833c3d55bee3e9321b70
vrdmv <viktor.dimov1@gmail.com>
2020-02-27 20:47:58 +0100

    Learning log initial commit

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\viktor\desktop\programming\python\python crash course\project iii - web applications\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
